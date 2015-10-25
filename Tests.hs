527c887d44d6d4cb8778bfd87d075f8e368511de
Scott Sadler <scott@alphanode.co>
2015-10-25 15:53:56 +0100

    tests, lexicographic ordering, still doesn't always complete

---
+++ Tests.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Test.Framework (defaultMain)
+import Test.Framework.Providers.HUnit
+import Test.HUnit
+
+import Math
+
+
+-- http://www.mesacc.edu/~scotz47781/mat120/notes/divide_poly/long_division/long_division_practice.html
+testDivide :: Test
+testDivide = TestLabel "Test Divide" $ TestList [
+    check "1" ("-1x3 -2x", "y3 -1y") (divide "-x4 +y3 -2x2 -y" "x"),
+    check "2" ("2x+3", "-8x+10") (divide "2x3-4x+7x2+7" "x2+2x-1"),
+    check "3" ("3x2-5x+6","") (divide "12x3-11x2+9x+18" "4x+3"),
+    check "4" ("-3y 2xy x","") (divide "x2+2x2y-2xy+2xy2-3y2" "x+y")
+    -- Never completes
+    -- check "5" ("","") (divide "x16+y20+2" "x10+y10-5")
+    ]
+  where check s a b = TestLabel s $ TestCase $ assertEqual "" a b
+
+
+testMul :: Assertion
+testMul = assertEqual "1 1"
+    ("x2 -x -2" ^* "3") "3x2 -3x -6"
+
+
+tests = hUnitTestToTests $ TestList [testDivide,
+                                     TestCase testMul]
+
+main :: IO ()
+main = defaultMain tests
+
--- src/Math/Terms.hs
+++ src/Math/Terms.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
 module Math.Terms where
 
 import Control.Applicative
 import Data.Attoparsec.Text as A
 import Data.Char (ord)
 import Data.Either (partitionEithers)
-import Data.List (sortBy)
+import Data.List (intercalate, sortBy)
 import qualified Data.Map as Map
-import Data.Maybe (catMaybes, listToMaybe)
+import Data.Maybe (catMaybes, listToMaybe, fromJust)
+import Data.Monoid
 import Data.Ord (comparing)
 import Data.String
 import Data.Text (Text, pack)
+import Debug.Trace
 
 -------------------------------------------------------------------------------
 -- Term (aka Monomial)
@@ -30,7 +35,7 @@ instance Show Term where
             ss k a = k : showExp a
 
 showExp :: Int -> String
-showExp e = if e == 1 then "" else map ("⁰¹²³⁴⁵⁶⁷⁸⁹"!!) digits
+showExp e = if e == 1 then "" else show e -- map ("⁰¹²³⁴⁵⁶⁷⁸⁹"!!) digits
   where digits = [ord c - 48 | c <- show e]
 
 
@@ -41,20 +46,23 @@ term co = Term co . Map.fromListWith (+)
 
 
 textToTerm :: Text -> Term
-textToTerm = either error id . parseOnly parseTerm
+textToTerm = either error id . parseOnly (parseTerm <* endOfInput)
+
+
+parseTerm :: Parser Term
+parseTerm = do
+    f <- skipSpace >> sign <* skipSpace
+    (took, co) <- (,) True <$> decimal <|> pure (False, 1)
+    vars <- many' variable <* skipSpace
+    let t = term (f co) vars
+    if took || vars /= [] then pure t else fail "No term"
+  where
+    sign = ("-" >> pure negate) <|> ("+" >> pure id) <|> pure id
+    variable = (,) <$> satisfy (inClass "a-z") <*> (decimal <|> pure 1)
 
 
 instance IsString Term where
     fromString = textToTerm . pack
-
-
-parseTerm :: Parser Term
-parseTerm = term <$> (signed decimal <|> pure 1)
-                 <*> many variable
-                  <* endOfInput
-  where
-    variable = (,) <$> satisfy (inClass "a-z")
-                   <*> (decimal <|> pure 1)
 
 
 data TermClass = TermClass String
@@ -63,8 +71,8 @@ data TermClass = TermClass String
 
 -- | Divide a term by another term, returning Nothing if they are
 -- not divisible into another (monomial) term
-termDiv :: Term -> Term -> Maybe Term
-termDiv (Term co1 dee) (Term co2 der) =
+termDivide :: Term -> Term -> Maybe Term
+termDivide (Term co1 dee) (Term co2 der) =
     let divideExp e1 e2 = if e1 > e2 then Just (e1 - e2) else Nothing
         vars = Map.differenceWith divideExp dee der
         sumDegree = Map.foldr (+) 0
@@ -74,8 +82,8 @@ termDiv (Term co1 dee) (Term co2 der) =
 
 
 -- | Multiply two terms
-termMul :: Term -> Term -> Term
-termMul (Term co1 vars1) (Term co2 vars2) = Term (co1*co2) vars3
+termMultiply :: Term -> Term -> Term
+termMultiply (Term co1 vars1) (Term co2 vars2) = Term (co1*co2) vars3
   where vars3 = Map.unionWith (+) vars1 vars2
 
 
@@ -85,6 +93,7 @@ termDegree (Term _ vars) = Map.foldr (+) 0 vars
 
 
 -- | Utility type to compare vars
+-- aka Lexicographic ordering
 newtype TermSig = TermSig TermVars deriving (Eq)
 instance Ord TermSig where
     compare = comparing (\(TermSig vars) -> Map.toAscList vars)
@@ -94,50 +103,71 @@ instance Ord TermSig where
 -- Polynomial
 ---------------------------------------------------------------------------------
 
-type Polynomial = [Term]
+newtype Polynomial = P [Term] deriving (Eq)
 
 
-polynomialMul :: Polynomial -> Polynomial -> Polynomial
-polynomialMul terms1 terms2 = polynomialReduce $ distribution
-  where distribution = [termMul t1 t2 | t1 <- terms1, t2 <- terms2]
+textToPolynomial :: Text -> Polynomial
+textToPolynomial = either error reduce . parseOnly parsePoly
+  where parsePoly = P <$> many' parseTerm <* endOfInput
+
+
+instance Monoid Polynomial
+  where
+    mappend (P a) (P b) = reduce $ P (a ++ b)
+    mempty = P [] 
+
+
+instance Show Polynomial
+    where show (P terms) = intercalate " " $ map show terms
+
+
+instance IsString Polynomial
+    where fromString = textToPolynomial . pack
+
+
+(^*) :: Polynomial -> Polynomial -> Polynomial
+(P terms1) ^* (P terms2) = reduce $ distribution
+  where distribution = P [termMultiply t1 t2 | t1 <- terms1, t2 <- terms2]
+infixl 7 ^*
 
 
 -- | Simplify a polynomial
 -- This is done by finding the terms with equal signatures and multiplying
 -- their coefficients.
-polynomialReduce :: Polynomial -> Polynomial
-polynomialReduce dist = polynomialSort reduced
-  where idx = Map.fromListWith (+) [(TermSig vars, co) | Term co vars <- dist]
+fix :: Polynomial -> Polynomial
+fix (P terms) = P $ sortBy (comparing cmp) reduced
+  where idx = Map.fromListWith (+) [(TermSig vars, co) | Term co vars <- terms]
         reduced = [Term co vars | (TermSig vars, co) <- Map.toList idx, co /= 0]
+        cmp (Term _ vars) = (- Map.foldr (+) 0 vars, - Map.foldr max 0 vars)
 
 
-polynomialSort :: Polynomial -> Polynomial
-polynomialSort = sortBy (comparing cmp)
-  where cmp (Term _ vars) = (- Map.foldr (+) 0 vars, - Map.foldr max 0 vars)
+-- | Reduce a polynomial. Returns in lexicographic order.
+reduce :: Polynomial -> Polynomial
+reduce (P terms) = P reduced
+  where idx = Map.fromListWith (+) [(TermSig vars, co) | Term co vars <- terms]
+        reduced = [Term co vars | (TermSig vars, co) <- Map.toDescList idx, co /= 0]
+
 
 -- | Divide polynomials
 -- Returns (quotient, remainder)
-dividePolynomial :: Polynomial -> Polynomial -> (Polynomial, Polynomial)
-dividePolynomial num den = (polynomialReduce lefts, polynomialReduce rights)
+divide :: Polynomial -> Polynomial -> (Polynomial, Polynomial)
+divide num den = (reduce $ P lefts, reduce $ P rights)
   where
-    (lefts, rights) = partitionEithers $ divide' (polynomialSort num) den
+    (lefts, rights) = partitionEithers $ divide' (reduce num) (fix den)
 
 
-divide' :: [Term] -> [Term] -> [Either Term Term]
-divide' [] _ = []
-divide' (t:ts) denominator =
+divide' :: Polynomial -> Polynomial -> [Either Term Term]
+divide' (P []) _                   = []
+divide' (P (t:ts)) (P denominator) =
         -- Only take the largest denominators.
     let maxDegree = foldr (max . termDegree) 0 denominator
         dens = filter ((== maxDegree) . termDegree) denominator
         -- Try to divide by any of the larger denominators
-        mres = listToMaybe $ catMaybes $ map (termDiv t) dens
+        mres = listToMaybe $ catMaybes $ map (termDivide t) dens
         -- Distribute the result
-        dist = case mres of
-            Just res -> polynomialMul (denominator) [res]
-            Nothing -> []
+        dist = P denominator ^* P [fromJust mres]
         -- Subtract to get the remainder
-        remainder = if dist == [] then ts
-                                  else polynomialReduce $ (t:ts) ++ polynomialMul ["-1"] dist
-        rest = divide' remainder denominator
-    in case mres of Nothing  -> Right t : rest
-                    Just res -> Left res : rest
+        remainder = P (t:ts) <> "-1" ^* dist
+    in case mres of
+        Nothing -> Right t : divide' (P ts) (P denominator)
+        Just res -> Left res : divide' remainder (P denominator)
